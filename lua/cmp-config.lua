c8bb593d4a729cfe8e8236bcda1bd3c3c35c1a84
Catherine Reprobate <catherine.reprobate@pm.me>
2026-08-30 14:42:36 -0700

    working lsp

--- lua/cmp-config.lua
+++ lua/cmp-config.lua
@@ -18,6 +18,42 @@ vim.lsp.config('*', {
 vim.lsp.enable('clangd')
 vim.lsp.enable('lua_ls')
 
+local builtin = require('telescope.builtin')
+
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', { clear = true }),
+  callback = function(ev)
+    -- Helper function to set keymaps with clean desc options
+    local map = function(mode, lhs, rhs, desc)
+      vim.keymap.set(mode, lhs, rhs, { buffer = ev.buf, silent = true, desc = desc })
+    end
+
+    -- Code Navigation (Standard gd, gD, gi, gt)
+    map('n', '<leader>lgd', vim.lsp.buf.definition, 'Go to Definition')
+    map('n', '<leader>lgD', vim.lsp.buf.declaration, 'Go to Declaration')
+    map('n', '<leader>lgi', vim.lsp.buf.implementation, 'Go to Implementation')
+    map('n', '<leader>lgt', vim.lsp.buf.type_definition, 'Go to Type Definition')
+    --
+    -- Jump to references across the whole codebase
+    vim.keymap.set('n', '<leader>lsr', builtin.lsp_references, { desc = 'Find References' })
+
+    -- Fuzzy search symbols (functions, structs, globals) in current file
+    vim.keymap.set('n', '<leader>lsd', builtin.lsp_document_symbols, { desc = 'Document Symbols' })
+
+    -- Fuzzy search symbols across the ENTIRE C project
+    vim.keymap.set('n', '<leader>lsw', builtin.lsp_workspace_symbols, { desc = 'Workspace Symbols' })
+
+    -- Hover & Documentation
+    map('n', 'K', vim.lsp.buf.hover, 'Hover Documentation')
+    map({ 'n', 'i' }, '<C-k>', vim.lsp.buf.signature_help, 'Signature Help')
+
+    -- Code Actions & Refactoring
+    map('n', '<leader>rn', vim.lsp.buf.rename, 'Rename Symbol')
+    map({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, 'Code Action')
+  end,
+})
+
 cmp.setup {
   snippet = {
     expand = function(args)
